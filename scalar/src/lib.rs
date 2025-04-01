520b91ebee930dffabd02ae7d910ba3d7b1b76f1
hilaokello <hilaryokello091@gmail.com>
2025-04-01 16:33:56 +0300

    add: initial solution to scalar

---
+++ scalar/src/lib.rs
@@ -0,0 +1,31 @@
+pub fn sum(a: u8, b: u8 X) -> u16 {
+    a + b
+}
+
+pub fn diff(a: i16, b: i16) -> i16 {
+    a - b
+}
+
+pub fn pro(a: i8, b: i8) -> i16 {
+    a * b
+}
+
+pub fn quo(a: i32, b: i32) -> i32 {
+    a / b
+
+}
+
+pub fn rem(a: i32, b: i32) -> i32 {
+    a % b
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }

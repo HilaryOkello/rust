4da52de0dc0d2a28e8bcb9eda88322dcd459ef42
HilaryOkello <hilaryokello091@gmail.com>
2025-04-03 22:09:31 +0300

    add string_literals

--- string_literals/src/lib.rs
+++ string_literals/src/lib.rs
@@ -1,14 +1,19 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn is_empty(v: &str) -> bool {
+    v,is_empty()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn is_ascii(v: &str) -> bool {
+    v.chars().all(|c| c.is_ascii())
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn contains(v: &str, pat: &str) -> bool {
+    v.contains(pat)
+}
+
+pub fn split_at(v: &str, index: usize) -> (&str, &str) {
+    v.split_at(index)
+}
+
+pub fn find(v: &str, pat: char) -> usize {
+    v.find(pat).unwrap_or(usize::MAX)
 }

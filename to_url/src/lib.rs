53ef5df1eccd97a66b40ce46d8641ac553c20184
HilaryOkello <hilaryokello091@gmail.com>
2025-04-03 22:06:04 +0300

    add string_literals

--- to_url/src/lib.rs
+++ to_url/src/lib.rs
@@ -1,5 +1,21 @@
-pub fn to_url(s: &str) -> String {
-    s.replace(" ", "%20")
+pub fn is_empty(v: &str) -> bool {
+    v,is_empty()
+}
+
+pub fn is_ascii(v: &str) -> bool {
+    v.chars().all(|c| c.is_ascii())
+}
+
+pub fn contains(v: &str, pat: &str) -> bool {
+    v.contains(pat)
+}
+
+pub fn split_at(v: &str, index: usize) -> (&str, &str) {
+    v.split_at(index)
+}
+
+pub fn find(v: &str, pat: char) -> usize {
+    v.find(pat).unwrap_or(usize::MAX)
 }
 
 #[cfg(test)]

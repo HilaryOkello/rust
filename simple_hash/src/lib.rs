b4c68092afcdf68e9c3041f7bc1c9f7482c46b70
HilaryOkello <hilaryokello091@gmail.com>
2025-04-05 21:37:04 +0300

    add simple_hash

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,10 +1,9 @@
 use std::collections::HashMap;
 
-pub fn word_frequency_counter<'a>(words: &'a Vec<&'a str>) -> HashMap<&'a str, usize> {
+pub fn word_frequency_counter<'a>(words: &'a [&'a str]) -> HashMap<&'a str, usize> {
     let mut frequency_map = HashMap::new();
-    for word in words {
-        let count = frequency_map.entry(word).or_insert(0);
-        *count += 1;
+    for &word in words {
+        *frequency_map.entry(word).or_insert(0) += 1;
     }
     frequency_map
 }

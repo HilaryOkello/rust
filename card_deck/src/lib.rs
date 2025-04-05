1c806d88e4d5559cefaab803c2123b053892bcce
HilaryOkello <hilaryokello091@gmail.com>
2025-04-05 20:10:02 +0300

    add card_deck

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
 
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Suit {
     Heart,
     Diamond,
@@ -8,27 +9,28 @@ pub enum Suit {
 }
 
 impl Suit {
-    pub fn random() -> Self {
-        match rand::thread_rng().gen_range(0..4) {
-            0 => Suit::Heart,
-            1 => Suit::Diamond,
-            2 => Suit::Spade,
-            3 => Suit::Club,
-            _ => unreachable!(),
+    pub fn random() -> Suit {
+        match rand::thread_rng().gen_range(1..=4) {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            _ => Suit::Club,
+  
         }
     }
 
-    pub fn translate(value: u8) -> Option<Self> {
+    pub fn translate(value: u8) -> Suit {
         match value {
-            1 => Some(Suit::Heart),
-            2 => Some(Suit::Diamond),
-            3 => Some(Suit::Spade),
-            4 => Some(Suit::Club),
-            _ => None,
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => panic!("Invalid suit value!"),
         }
     }
 }
 
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Rank {
     Ace,
     King,
@@ -48,82 +50,27 @@ impl Rank {
         }
     }
 
-    pub fn translate(value: u8) -> Option<Self> {
+    pub fn translate(value: u8) -> Rank {
         match value {
-            1 => Some(Rank::Ace),
-            n @ 2..=10 => Some(Rank::Number(n)),
-            11 => Some(Rank::Jack),
-            12 => Some(Rank::Queen),
-            13 => Some(Rank::King),
-            _ => None,
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => panic!("Invalid rank value!"),
         }
     }
 }
 
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
 }
 
-pub fn winner_card(card: &Card) -> bool {
-    match (&card.suit, &card.rank) {
-        (Suit::Spade, Rank::Ace) => true,
-        _ => false,
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_suit_random() {
-        for _ in 0..100 {
-            let suit = Suit::random();
-            match suit {
-                Suit::Heart | Suit::Diamond | Suit::Spade | Suit::Club => assert!(true),
-            }
-        }
-    }
-
-    #[test]
-    fn test_suit_translate() {
-        assert!(matches!(Suit::translate(1), Some(Suit::Heart)));
-        assert!(matches!(Suit::translate(2), Some(Suit::Diamond)));
-        assert!(matches!(Suit::translate(3), Some(Suit::Spade)));
-        assert!(matches!(Suit::translate(4), Some(Suit::Club)));
-        assert!(Suit::translate(0).is_none());
-        assert!(Suit::translate(5).is_none());
-    }
-
-    #[test]
-    fn test_rank_random() {
-        for _ in 0..100 {
-            let rank = Rank::random();
-            match rank {
-                Rank::Ace | Rank::King | Rank::Queen | Rank::Jack | Rank::Number(..) => assert!(true),
-            }
-        }
-    }
-
-    #[test]
-    fn test_rank_translate() {
-        assert!(matches!(Rank::translate(1), Some(Rank::Ace)));
-        assert!(matches!(Rank::translate(2), Some(Rank::Number(2))));
-        assert!(matches!(Rank::translate(10), Some(Rank::Number(10))));
-        assert!(matches!(Rank::translate(11), Some(Rank::Jack)));
-        assert!(matches!(Rank::translate(12), Some(Rank::Queen)));
-        assert!(matches!(Rank::translate(13), Some(Rank::King)));
-        assert!(Rank::translate(0).is_none());
-        assert!(Rank::translate(14).is_none());
-    }
-
-    #[test]
-    fn test_winner_card() {
-        let ace_of_spades = Card { suit: Suit::Spade, rank: Rank::Ace };
-        let king_of_hearts = Card { suit: Suit::Heart, rank: Rank::King };
-
-        assert!(winner_card(&ace_of_spades));
-        assert!(!winner_card(&king_of_hearts));
+pub fn winner_card(card: Card) -> bool {
+    card == Card {
+        suit: Suit::Spade,
+        rank: Rank::Ace,
     }
 }

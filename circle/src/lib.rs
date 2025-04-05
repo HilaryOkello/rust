52ee5d41f7899c899fd420789a94d95fa770c9ad
HilaryOkello <hilaryokello091@gmail.com>
2025-04-05 19:25:17 +0300

    add circles

---
+++ circle/src/lib.rs
@@ -0,0 +1,44 @@
+use std::f64::consts::PI;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Point {
+    pub fn new(x: f64, y: f64) -> Self {
+        Point { x, y }
+    }
+
+    pub fn distance(&self, other: &Point) -> f64 {
+        ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Circle {
+    pub center: Point,
+    pub radius: f64,
+}
+
+impl Circle {
+    pub fn new(x: f64, y: f64, radius: f64) -> Self {
+        Circle {
+            center: Point::new(x, y),
+            radius,
+        }
+    }
+
+    pub fn diameter(&self) -> f64 {
+        self.radius * 2.0
+    }
+
+    pub fn area(&self) -> f64 {
+        PI * self.radius.powi(2)
+    }
+
+    pub fn intersect(&self, other: &Circle) -> bool {
+        self.center.distance(&other.center) <= self.radius + other.radius
+    }
+}

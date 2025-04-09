ef60c910adfcb83f548b46fc2f1f76f1968a9563
hilaokello <hilaryokello091@gmail.com>
2025-04-09 14:57:25 +0300

    add profanity filter

---
+++ profanity_filter/src/lib.rs
@@ -0,0 +1,29 @@
+struct Message {
+    content: String,
+    user: String,
+}
+
+//new initializes Message
+impl Message {
+    pub fn new(content: String, user: String) -> Self {
+        Message { content, user }
+    }
+
+    pub fn send_ms(self) -> Option<String> {
+        if self.content.is_empty() || self.content.contains("stupid") {
+            None
+        } else {
+            Some(self.content)
+        }
+    }
+}
+
+pub fn check_ms(message: &str) -> Result<&str, &str> {
+    msg :=  Mesage::new(message.to_string(), "user".to_string());
+
+    match msg.send_ms() {
+        Some(_) => Ok("Message sent"),
+        None => Err("Message contains profanity"),
+    }
+
+}

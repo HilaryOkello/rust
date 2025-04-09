8578e1e2c9cb8072a8418a57ef74400ceb718b8d
HilaryOkello <hilaryokello091@gmail.com>
2025-04-09 21:01:17 +0300

    add error types

---
+++ error_types/src/lib.rs
@@ -0,0 +1,45 @@
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct FormError {
+    form_values: Vec<(String, String)>,
+    date: String,
+    err: String,
+}
+
+impl FormError {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
+        let date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        FormError {
+            form_values: vec![(field_name.to_string(), field_value)],
+            date,
+            err: err.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    name: String,
+    password: String
+}
+
+impl Form {
+    pub fn validate(&self) -> Result<(), FormError> {
+        if self.name.is_empty() {
+            return Err(FormError::new("name", self.name.clone(), "Username is empty"));
+        }
+        if self.password.len() < 8 {
+            return Err(FormError::new("password", self.password.clone(), "Password should be at least 8 characters long"));
+        }
+        if !self.password.chars().any(|c| c.is_ascii_alphanumeric()) {
+            return Err(FormError::new("password", self.password.clone(), "Password must contain at least one alphanumeric character"));
+        }
+        if !self.password.chars().any(|c| c.is_ascii_digit()) {
+            return Err(FormError::new("password", self.password.clone(), "Password should be a combination of ASCII numbers, letters and symbols"));
+        }
+        if !self.password.chars().any(|c| c.is_ascii_punctuation()) {
+            return Err(FormError::new("password", self.password.clone(), "Password should be a combination of ASCII numbers, letters and symbols"));
+        }
+        Ok(()) 
+    }
+}

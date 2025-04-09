752e262e8432858f5fc631641757a8e7a1e71d99
hilaokello <hilaryokello091@gmail.com>
2025-04-09 15:46:36 +0300

    add question mark

---
+++ question_mark/src/lib.rs
@@ -0,0 +1,26 @@
+pub struct Open {
+    first_layer: Option<Two>
+}
+
+
+pub struct Two {
+    second_layer: Option<Three>
+}
+
+
+pub struct Three {
+    third_layer: Option<Four>
+}
+
+
+pub struct Four {
+    fourth_layer: Option<u16>
+}
+
+impl One {
+    pub fn get_fourth_layer(self) -> Option<u16> {
+        let two = self.first_layer?;
+        let three = two.second_layer?;
+        let four = three.third_layer?;
+        four.fourth_layer
+}

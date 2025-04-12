6a2437fb2a2f5784b338421a261eb4ef71a9a9c7
HilaryOkello <hilaryokello091@gmail.com>
2025-04-12 17:51:05 +0300

    add talking

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,14 +1,15 @@
 pub fn talking(text: &str) -> &str {
-    if text.is_empty() {
+    let trimmed_text = text.trim();
+    if trimmed_text.is_empty() {
         return "Just say something!";
     }
-    let is_yelling = text.chars().any(char::is_alphabetic) && text.chars().all(|c| !c.is_alphabetic() || c.is_uppercase());
-    let is_question = text.ends_with('?');
+    let is_yelling = trimmed_text.chars().any(char::is_alphabetic) && trimmed_text.chars().all(|c| !c.is_alphabetic() || c.is_uppercase());
+    let is_question = trimmed_text.ends_with('?');
 
     match (is_question, is_yelling) {
         (true, true) => "Quiet, I am thinking!",
         (true, false) => "Sure.",
         (false, true) => "There is no need to yell, calm down!",
-        (false, false) => "Interesting",
+        (false, false) => "Interesting.",
     }
 }

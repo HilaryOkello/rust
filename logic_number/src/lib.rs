0503ee858e137bf0fd4409b845c633daa9468733
HilaryOkello <hilaryokello091@gmail.com>
2025-04-12 18:07:47 +0300

    add logic number

--- logic_number/src/lib.rs
+++ logic_number/src/lib.rs
@@ -1,12 +1,12 @@
 pub fn number_logic(num: u32) -> bool {
     let len = num.to_string().len();
-    let mut num = num;
+    let mut n = num
     let mut sum = 0;
 
-    while num > 0 {
-        let digit = num % 10;
+    while n > 0 {
+        let digit = n % 10;
         sum += digit.pow(len as u32);
-        num /= 10;
+        n /= 10;
     }
     sum == num
 }

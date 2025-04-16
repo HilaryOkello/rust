256a8d854ed1d004374954272841b8f3a519800f
hilaokello <hilaryokello091@gmail.com>
2025-04-16 16:47:46 +0300

    add recursion

---
+++ box_recursion/src/lib.rs
@@ -0,0 +1,39 @@
+#[derive(Debug)]
+pub struct WorkEnvironment {
+    pub grade: Link,
+}
+
+pub type Link = Option<Box<Worker>>;
+
+#[derive(Debug)]
+pub struct Worker {
+    pub role: String,
+    pub name: String,
+    pub next: Link,
+}
+
+impl WorkEnvironment {
+    pub fn new() -> WorkEnvironment {
+        WorkEnvironment { grade: None }
+    }
+
+    pub fn add_worker(&mut self, role: String, name: String) {
+        let new_worker = Box::new(Worker {
+            role,
+            name,
+            next: self.grade.take(),
+        });
+        self.grade = Some(new_worker);
+    }
+
+    pub fn remove_worker(&mut self) -> Option<String> {
+        self.grade.take().map(|node| {
+            self.grade = node.next;
+            node.name
+        })
+    }
+
+    pub fn last_worker(&self) -> Option<(String, String)> {
+        self.grade.as_ref().map(|node| (node.name.clone(), node.role.clone()))
+    }
+}
